5cc29a887791791eaa074ad5b5b35f5d1b71883d
Wagner <wagner.851@CIO096677.local>
2016-05-04 13:48:42 -0400

    Added CSS and simplistic Angular shenanigans

--- main.js
+++ main.js
@@ -1,13 +1,31 @@
 $(document).ready(function(){
   var $content = $('.content');
-  $.ajax({
-    type: 'GET',
-    url: 'testAPI.php',
-    success: function(people){
-      $.each(people,function(i,person){
-          $content.append(person.name+' goes to '+person.school+'. He is '+person.age+' years old. Does he like bagels? '+person.bagels+'.</br>');
-      });
-      }
-    });
+  var $click = $('.click');
+
+  function getPerson(){
+    var personID = $click.html();
+    if(personID){
+      $.ajax({
+        type: 'GET',
+        url: 'testAPI.php',
+        success: function(people){
+          if(Object.keys(people).length <= personID){
+            $content.empty();
+            $content.append('ID does not exist.');
+          }
+          else{
+            $.each(people,function(i,person){
+                if(person.id==personID){
+                  $content.empty();
+                  $content.append(person.name+' goes to '+person.school+'. He is '
+                  +person.age+' years old. Does he like bagels? '+person.bagels+'.</br>');
+                }
+            });
+          }
+          }
+        });
+    }
+  };
+  $click.on('click',getPerson);
 
 }); //end of jQ
--- testAPI.php
+++ testAPI.php
@@ -3,7 +3,8 @@ header("Content-Type: application/json; charset=utf-8");
 // test API to see what's crack-a-lackin'
 $students = array(
   array('id'=>0,'name'=>'Hunt','student'=>'yes','school'=>'Ohio State','age'=>25,'bagels'=>'Yes'),
-  array('id'=>1,'name'=>'Timmy','student'=>'yes','school'=>'Michigan','age'=>15,'bagels'=>'No')
+  array('id'=>1,'name'=>'Timmy','student'=>'yes','school'=>'Michigan','age'=>15,'bagels'=>'No'),
+  array('id'=>2,'name'=>'Ben','student'=>'yes','school'=>'Northwestern','age'=>19,'bagels'=>'No'),
 );
 
 
