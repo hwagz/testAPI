122459d01da8380b5dffe838cf917f15edf87197
hwagz <hunt.wagner@gmail.com>
2016-05-04 21:24:51 -0400

    Minor value changes

--- testAPI.php
+++ testAPI.php
@@ -2,9 +2,9 @@
 header("Content-Type: application/json; charset=utf-8");
 // test API to see what's crack-a-lackin'
 $students = array(
-  array('id'=>0,'name'=>'Hunt','student'=>'yes','school'=>'Ohio State','age'=>25,'bagels'=>'Yes'),
-  array('id'=>1,'name'=>'Timmy','student'=>'yes','school'=>'Michigan','age'=>15,'bagels'=>'No'),
-  array('id'=>2,'name'=>'Ben','student'=>'yes','school'=>'Northwestern','age'=>19,'bagels'=>'No'),
+  array('id'=>0,'name'=>'Hunt','student'=>true,'school'=>'Ohio State','age'=>25,'bagels'=>'Yes'),
+  array('id'=>1,'name'=>'Timmy','student'=>true,'school'=>'Michigan','age'=>15,'bagels'=>'No'),
+  array('id'=>2,'name'=>'Ben','student'=>true,'school'=>'Northwestern','age'=>19,'bagels'=>'No'),
 );
 
 
